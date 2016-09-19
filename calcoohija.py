123784a2554115af4a2dd7cf1b6272df68fa9e42
Oscar Martin Herrera <omartin@gamma19.aulas.gsyc.urjc.es>
2016-09-19 15:36:31 +0200

    Practica 2 punto 4.5

---
+++ calcoohija.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+
+class CalculadoraHija(calcoo.Calculadora):
+    
+    def mult (self, op1, op2):
+        return op1 * op2
+        
+    def div (self, op1, op2):
+        return op1 / op2        
+          
+if __name__ == "__main__":
+
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])     
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    calculadora = CalculadoraHija()
+        
+    if  sys.argv[2] == "resta":
+        resultado=calculadora.resta(operando1,operando2)
+    elif sys.argv[2]=="suma":
+        resultado=calculadora.suma(operando1,operando2)
+    elif sys.argv[2]=="divide":
+        if sys.argv[3] == "0":
+            sys.exit("Division by zero is not allowed")
+        else:
+           resultado = calculadora.div(operando1, operando2)
+    elif sys.argv[2]=="multiplica":
+        resultado = calculadora.mult(operando1, operando2)         
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+        
+    print(resultado)
+    
+
