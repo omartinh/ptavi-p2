e96dfb37446cf410cc646d5c1410ebe7aedf0e23
Oscar Martin Herrera <omartin@alpha09.aulas.gsyc.urjc.es>
2016-09-19 12:47:57 +0200

    P2 punto 4.4

---
+++ calcoo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+
+    def suma (self, op1, op2):
+        return op1 + op2
+        
+    def resta (self, op1 , op2):
+        return op1 - op2
+        
+          
+if __name__ == "__main__":
+    
+    operando1 = int(sys.argv[1])
+    operando2 = int(sys.argv[3])
+    calculadora=Calculadora()
+    if  sys.argv[2] == "resta":
+        resultado=calculadora.resta(operando1,operando2)
+    elif sys.argv[2]=="suma":
+        resultado=calculadora.suma(operando1,operando2)
+    print(resultado)
+    
+
