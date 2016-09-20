b13eb89ae78a44d6c4fd59e2e437e348a12f47dd
Oscar Martin Herrera <omartin@alpha30.aulas.gsyc.urjc.es>
2016-09-20 11:38:55 +0200

    Practica2 punto4.6

--- calcplus.py
+++ calcplus.py
@@ -3,23 +3,50 @@
 
 import sys
 import calcoohija
-import calcoo
 
-calculadora=Calculadora # problemas a la hora de operar
+calculadora=calcoohija.CalculadoraHija() # problemas a la hora de operar
 fichero = open('fich_operaciones','r')
 lineas = fichero.readlines()
 
 for linea in lineas:
     elem = linea.split(',')
     operacion = elem[0]
-    num1 = elem[1]
-    num2=elem[2]
+    num1 = int(elem[1])
+    num2=int(elem[2])
+    nums=elem[3:]
     
     if operacion == "suma":
-        result = calcoo.suma(num1,num2)
-        for signum in linea:
-           result = result+ signum
-            
+        print("___esta es la suma___")
+        result = calculadora.suma(num1,num2)
+        for num in nums: 
+           result = result + int(num)
+        print(result)
         
-print(result) 
+    elif operacion == "resta":
+        print("___esta es la resta___")
+        result = calculadora.resta(num1,num2)
+        for num in nums:
+            result = result - int(num)
+        print(result)
+        
+    elif operacion == "multiplica":
+        print("___esta es la multiplicacion___")
+        result = calculadora.mult(num1,num2)
+        for num in nums:
+            result = result * int(num)
+        print(result)
+        
+    elif operacion == "divide":
+        print("___esta es la division___")
+        if num2!=0:
+            result = calculadora.div(num1,num2)
+            for num in nums:
+                if int(num)!= 0 :
+                    result = result / int(num)
+                    print(result)
+                else:
+                   print("Division by zero is not allowed")     
+        else: 
+            print("Division by zero is not allowed")    
+
 
