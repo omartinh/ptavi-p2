8efe3852a568843bf527f7624cac0165e38ea2ea
Oscar Martin Herrera <omartin@alpha18.aulas.gsyc.urjc.es>
2016-09-26 13:56:02 +0200

    Practica2 ejercicio 4.6

--- calcplus.py
+++ calcplus.py
@@ -4,57 +4,47 @@
 import sys
 import calcoohija
 
-fich=sys.argv[1]
-calculadora=calcoohija.CalculadoraHija() #problemas a la hora de operar
+fich = sys.argv[1]
+calculadora = calcoohija.CalculadoraHija()  # problemas a la hora de operar
 
 if __name__ == "__main__":
 
     try:
-        fichero = open(fich,'r')
+        fichero = open(fich, 'r')
     except:
         sys.exit("Error: fich name not found")
 
-    
-    lineas = fichero.readlines()   
-    
+    lineas = fichero.readlines()
+
     for linea in lineas:
         elem = linea.split(',')
         operacion = elem[0]
         num1 = int(elem[1])
-        num2=int(elem[2])
-        nums=elem[3:]
-    
+        nums = elem[2:]
+
         if operacion == "suma":
-            result = calculadora.suma(num1,num2)
-            for num in nums: 
-                result = calculadora.suma(result,int(num))
-            print(result)
-        
-        elif operacion == "resta":
-            if(num1>num2):
-                result = calculadora.resta(num1,num2)
-                for num in nums:
-                    result = calculadora.resta(result,int(num))
-                print(result)
-            else:
-                print("Error operandos resta" )
-        
-        elif operacion == "multiplica":
-            result = calculadora.mult(num1,num2)
+            result = num1
             for num in nums:
-                result = calculadora.mult(result,int(num))
+                result = calculadora.suma(result, int(num))
             print(result)
-        
-        elif operacion == "divide": 
-            if num2!=0:
-                result = calculadora.div(num1,num2)
-                for num in nums:
-                    if int(num)!= 0 :
-                        result = calculadora.div(result,int(num))
-                        print(result)
-                    else:
-                        print("Division by zero is not allowed")     
-            else: 
-                print("Division by zero is not allowed")    
-
-
+        elif operacion == "resta":
+            result = num1
+            for num in nums:
+                if(num1 > int(num)):
+                    result = calculadora.resta(result, int(num))
+                else:
+                    result = 0  # cuando el primer valor es menor que el segundo dara 0
+            print(result)
+        elif operacion == "multiplica":
+            result = num1
+            for num in nums:
+                result = calculadora.mult(result, int(num))
+            print(result)
+        elif operacion == "divide":
+            result = num1
+            for num in nums:
+                if int(num) != 0:
+                    result = calculadora.div(result, int(num))
+                else:
+                    sys.exit("Division by zero is not allowed")
+            print(result)
