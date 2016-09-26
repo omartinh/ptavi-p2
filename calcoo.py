f372051c5b829472f61cd9fa84e8d86aae928d6c
Oscar Martin Herrera <omartin@alpha18.aulas.gsyc.urjc.es>
2016-09-26 12:39:24 +0200

    Practica 2 ejercicio 4.4

--- calcoo.py
+++ calcoo.py
@@ -3,31 +3,30 @@
 
 import sys
 
+
 class Calculadora():
 
-    def suma (self, op1, op2):
+    def suma(self, op1, op2):
         return op1 + op2
-        
-    def resta (self, op1 , op2):
+
+    def resta(self, op1, op2):
         return op1 - op2
-        
-          
+
+
 if __name__ == "__main__":
 
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
-        calculadora=Calculadora()
-        
+        calculadora = Calculadora()
+
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-        
-    if  sys.argv[2] == "resta":
-        resultado=calculadora.resta(operando1,operando2)
-    elif sys.argv[2]=="suma":
-        resultado=calculadora.suma(operando1,operando2) 
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')      
-    print(resultado)
-    
 
+    if sys.argv[2] == "resta":
+        resultado = calculadora.resta(operando1, operando2)
+    elif sys.argv[2] == "suma":
+        resultado = calculadora.suma(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+    print(resultado)
